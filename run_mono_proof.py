303349c7b3da2f6eec713c2023dda2358dec9737
agithubuserseva <sevadonacube@gmail.com>
2023-01-31 18:37:11 -0500

    sync

--- run_mono_proof.py
+++ run_mono_proof.py
@@ -7,6 +7,7 @@ if __name__ == "__main__":
 
     backward_check = True
     lemma_bitblast = False
+    graph_reduction = True
     for i, arg in enumerate(sys.argv):
         if sys.argv[i].startswith("--no-backward-check"):
             backward_check = False
@@ -19,6 +20,12 @@ if __name__ == "__main__":
             del (sys.argv[i])
             break
 
+    for i, arg in enumerate(sys.argv):
+        if sys.argv[i].startswith("--no-graph-reduction"):
+            graph_reduction = False
+            del (sys.argv[i])
+            break
+
     r = Record("test")
     run_and_prove(input_gnf, r, running_opt=[], witness_reduction=False, backward_check=backward_check,
-                  lemma_bitblast=lemma_bitblast)
+                  lemma_bitblast=lemma_bitblast, graph_reduction=graph_reduction)
