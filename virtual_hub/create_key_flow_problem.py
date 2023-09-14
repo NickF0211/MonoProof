0379591f4901098c93e9eb38848717d7f3ecf772
agithubuserseva <sevadonacube@gmail.com>
2023-09-14 14:29:56 -0400

    update benchmark diff

--- virtual_hub/create_key_flow_problem.py
+++ virtual_hub/create_key_flow_problem.py
@@ -3,7 +3,7 @@ import subprocess
 from virtual_hub import tgw_instance, reset
 
 if __name__ == "__main__":
-    for size_lb in range(6, 8):
+    for size_lb in range(6, 10):
         for size_ub in range(size_lb, size_lb+2):
             for prob in range(5, 11):
                 prob = prob / 10
